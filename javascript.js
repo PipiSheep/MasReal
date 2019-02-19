55157c7bc81553ee57adb5f15d2f2aa2a5daf1e8
PipiSheep <cristina__borrego@hotmail.com>
2019-02-19 12:18:09 +0100

    v2.0

---
+++ javascript.js
@@ -0,0 +1,227 @@
+function cambiar(a){
+    if (a == "mas"){
+        for(i=1 ; i<9; i++){
+            document.getElementById("m"+i).src="fotos/marca"+String(i+8)+".png";
+        }
+    }else{
+        for(i=1 ; i<9; i++){
+            document.getElementById("m"+i).src="fotos/marca"+String(i)+".png";
+        }
+    }
+}
+function rulafoto(a){
+    var fotos = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/gelectricas/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/gelectricas/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/gelectricas/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/gelectricas/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/gelectricas/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/gelectricas/"+String(fotos[5]);
+    }
+}
+
+function rulafoto1(a){
+    var fotos = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/gelectricas1/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/gelectricas1/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/gelectricas1/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/gelectricas1/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/gelectricas1/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/gelectricas1/"+String(fotos[5]);
+    }
+}
+
+function rulafoto2(a){
+    var fotos = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/gelectricas2/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/gelectricas2/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/gelectricas2/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/gelectricas2/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/gelectricas2/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/gelectricas2/"+String(fotos[5]);
+    }
+}
+
+function rulafoto3(a){
+    var fotos = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/gelectricas3/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/gelectricas3/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/gelectricas3/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/gelectricas3/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/gelectricas3/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/gelectricas3/"+String(fotos[5]);
+    }
+}
+
+function rulafoto4(a){
+    var fotos = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/gelectricas4/"+Strin
+        var ñjkasdñf=6;(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/gelectricas4/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/gelectricas4/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/gelectricas4/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/gelectricas4/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/gelectricas4/"+String(fotos[5]);
+    }
+}
+
+function rulafoto5(a){
+    var fotos = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/gelectricas"+añskjdfhñkas+"/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/gelectricas5/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/gelectricas5/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/gelectricas5/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/gelectricas5/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/gelectricas5/"+String(fotos[5]);
+    }
+}
+
+function rulafoto6(a){
+    var fotos = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/gelectricas6/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/gelectricas6/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/gelectricas6/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/gelectricas6/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/gelectricas6/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/gelectricas6/"+String(fotos[5]);
+    }
+}
+
+function rulafoto7(a){
+    var fotos = ["0.png","1.png","2.png","3.png","4.png","5.png"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/bAcustica/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/bAcustica/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/bAcustica/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/bAcustica/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/bAcustica/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/bAcustica/"+String(fotos[5]);
+    }
+}
+
+function rulafoto8(a){
+    var fotos = ["0.png","1.png","2.png","3.png","4.png","5.png"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/bAcustica1/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/bAcustica1/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/bAcustica1/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/bAcustica1/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/bAcustica1/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/bAcustica1/"+String(fotos[5]);
+    }
+}
+
+function rulafoto9(a){
+    var fotos = ["0.png","1.png","2.png","3.png","4.png","5.png"];
+    if(a == 'foto1'){
+        document.getElementById('fotopro').src = "fotos/bAcustica2/"+String(fotos[0]);
+    }else if(a == 'foto2'){
+        document.getElementById('fotopro').src = "fotos/bAcustica2/"+String(fotos[1]);
+    }else if(a == 'foto3'){
+        document.getElementById('fotopro').src = "fotos/bAcustica2/"+String(fotos[2]);
+    }else if(a == 'foto4'){
+        document.getElementById('fotopro').src = "fotos/bAcustica2/"+String(fotos[3]);
+    }else if(a == 'foto5'){
+        document.getElementById('fotopro').src = "fotos/bAcustica2/"+String(fotos[4]);
+    }else if(a == 'foto6'){
+        document.getElementById('fotopro').src = "fotos/bAcustica2/"+String(fotos[5]);
+    }
+}
+
+function cliente(id){
+if(id == 71){
+    document.getElementById(81).removeAttribute("hidden","");
+    document.getElementById(82).setAttribute("hidden","");
+    document.getElementById(83).setAttribute("hidden","");
+    document.getElementById(84).setAttribute("hidden","");
+    document.getElementById(85).setAttribute("hidden","");
+    document.getElementById(86).setAttribute("hidden","");
+}else if(id == 72){
+    document.getElementById(81).setAttribute("hidden","");
+    document.getElementById(82).removeAttribute("hidden","");
+    document.getElementById(83).setAttribute("hidden","");
+    document.getElementById(84).setAttribute("hidden","");
+    document.getElementById(85).setAttribute("hidden","");
+    document.getElementById(86).setAttribute("hidden","");
+}else if(id == 73){
+    document.getElementById(81).setAttribute("hidden","");
+    document.getElementById(82).setAttribute("hidden","");
+    document.getElementById(83).removeAttribute("hidden","");
+    document.getElementById(84).setAttribute("hidden","");
+    document.getElementById(85).setAttribute("hidden","");
+    document.getElementById(86).setAttribute("hidden","");
+}else if(id == 74){
+    document.getElementById(81).setAttribute("hidden","");
+    document.getElementById(82).setAttribute("hidden","");
+    document.getElementById(83).setAttribute("hidden","");
+    document.getElementById(84).removeAttribute("hidden","");
+    document.getElementById(85).setAttribute("hidden","");
+    document.getElementById(86).setAttribute("hidden","");
+}else if(id == 75){
+    document.getElementById(81).setAttribute("hidden","");
+    document.getElementById(82).setAttribute("hidden","");
+    document.getElementById(83).setAttribute("hidden","");
+    document.getElementById(84).setAttribute("hidden","");
+    document.getElementById(85).removeAttribute("hidden","");
+    document.getElementById(86).setAttribute("hidden","");
+}else if(id == 76){
+    document.getElementById(81).setAttribute("hidden","");
+    document.getElementById(82).setAttribute("hidden","");
+    document.getElementById(83).setAttribute("hidden","");
+    document.getElementById(84).setAttribute("hidden","");
+    document.getElementById(85).setAttribute("hidden","");
+    document.getElementById(86).removeAttribute("hidden","");
+}
+}
